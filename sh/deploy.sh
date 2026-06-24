831021c539aceaa298b9cec30bb085be5625e825
myjyby <myjyby@gmail.com>
2026-06-24 15:04:33 +0200

    added deploy script

---
+++ sh/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../" &> /dev/null
+
+if ! make -s git-check ; then
+    echo "working directory needs to be clean to deploy"
+    exit 1
+fi
+
+BRANCH_MAIN=master
+
+if [ $(make -s branch) != "${BRANCH_MAIN}" ]; then
+    echo "must be on ${BRANCH_MAIN} to deploy"
+    exit 2
+fi
+
+git fetch --tags
+TAG=$(make -s next-version)
+
+if [ $(git tag --points-at HEAD) ]; then
+    echo "commit is already deployed!"
+    exit 3
+fi
+
+echo "deploying version: ${TAG}"
+git tag "${TAG}"
+git push --tags
